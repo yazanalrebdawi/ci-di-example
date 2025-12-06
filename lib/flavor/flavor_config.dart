26f69e82038aec5fa6a5039339ef2904505e82c1
yazanalrebdawi <rebdawi2002@gmail.com>
2025-12-06 14:50:59 +0300

    mm

--- lib/flavor/flavor_config.dart
+++ lib/flavor/flavor_config.dart
@@ -3,10 +3,10 @@ enum Flavor { dev, staging, production }
 class FlavorConfig {
   final Flavor flavor;
   final String name;
-  final String baseUrl; 
+  final String baseUrl;
   static FlavorConfig? _instance;
   FlavorConfig._(
-      {required this.flavor, required this.name, required this.baseUrl,});
+      {required this.flavor, required this.name, required this.baseUrl});
 
   factory FlavorConfig(
       {required Flavor flavor, required String name, required String baseUrl}) {
@@ -20,9 +20,7 @@ class FlavorConfig {
     return _instance!;
   }
 
-static bool get isDev => instance.flavor == Flavor.dev;
-static bool get isStaging => instance.flavor == Flavor.staging;
-static bool get isProduction => instance.flavor == Flavor.production; 
-
-
+  static bool get isDev => instance.flavor == Flavor.dev;
+  static bool get isStaging => instance.flavor == Flavor.staging;
+  static bool get isProduction => instance.flavor == Flavor.production;
 }
